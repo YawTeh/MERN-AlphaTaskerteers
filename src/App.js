b38e3257adedfe548bdaec302f115c432eac333c
Yaw Teh <yawteh@gmail.com>
2021-05-17 19:21:38 -0400

    Added Reminder Toggle Feature

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
-import { useState } from 'react';
-import Header from './components/Header';
-import Tasks from './components/Tasks';
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
-    const [tasks, setTasks] = useState ([
+    const [tasks, setTasks] = useState([
         {
             id: 1,
-            text: 'Doctors Appointment', 
-            day: 'Feb 5th at 2:30pm', 
+            text: "Doctors Appointment",
+            day: "Feb 5th at 2:30pm",
             reminder: true,
         },
         {
             id: 2,
-            text: 'Metting at School', 
-            day: 'Feb 6th at 1:30pm', 
+            text: "Metting at School",
+            day: "Feb 6th at 1:30pm",
             reminder: true,
         },
         {
             id: 3,
-            text: 'Food Shopping', 
-            day: 'Feb 5th at 2:30pm', 
+            text: "Food Shopping",
+            day: "Feb 5th at 2:30pm",
             reminder: false,
-        }
-    ])
+        },
+    ]);
 
     const deleteTask = (id) => {
-        setTasks(tasks.filter((task) => task.id !== id))
-    }
+        setTasks(tasks.filter((task) => task.id !== id));
+    };
+
+    const toggleReminder = (id) => {
+        setTasks(
+            tasks.map((task) =>
+                task.id === id ? { ...task, reminder : !task.reminder } : task
+            )
+        );
+    };
 
     return (
         <div className="container">
-            <Header/>
-            {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : ('No Tasks To Show')}
+            <Header />
+            {tasks.length > 0 ? (
+                <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                />
+            ) : (
+                "No Tasks To Show"
+            )}
         </div>
     );
 }
