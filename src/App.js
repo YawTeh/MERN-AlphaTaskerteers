c3cba507b1b1bd97c1f0cb96667943e041b42498
Yaw Teh <yawteh@gmail.com>
2021-05-24 22:53:54 -0400

    Added Toggleable Menu

--- src/App.js
+++ src/App.js
@@ -4,26 +4,8 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: "Doctors Appointment",
-            day: "Feb 5th at 2:30pm",
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: "Metting at School",
-            day: "Feb 6th at 1:30pm",
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: "Food Shopping",
-            day: "Feb 5th at 2:30pm",
-            reminder: false,
-        },
-    ]);
+    const [showAddTask, setShowAddTask] = useState (false)
+    const [tasks, setTasks] = useState([]);
 
     const addTask = (task) => {
         const id = Math.floor(Math.random() * 10000) + 1
@@ -43,10 +25,11 @@ function App() {
         );
     };
 
+
     return (
         <div className="container">
-            <Header />
-            <AddTask onAdd={addTask} />
+            <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+            {showAddTask && <AddTask onAdd={addTask} showAdd={showAddTask} />}
             {tasks.length > 0 ? (
                 <Tasks
                     tasks={tasks}
