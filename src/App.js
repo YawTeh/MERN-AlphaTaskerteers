d74ae8bad085ed54c0d80b6d5aeac4a246f1a46e
Yaw Teh <yawteh@gmail.com>
2021-05-25 23:43:43 -0400

    Fixed About Component Showing on Default

--- src/App.js
+++ src/App.js
@@ -108,7 +108,6 @@ function App() {
                             )}
                         </>
                     )}
-                    component={About}
                 />
                 <Route path="/about" component={About} />
                 <Footer />
